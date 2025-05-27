23f288b0b36c24b3ee3afde6f676b2be02de8953
Muzammil-EDF <123097477+Muzammil-EDF@users.noreply.github.com>
2025-05-27 15:48:26 +0500

    Update Home.py

--- APP/Home.py
+++ APP/Home.py
@@ -2,13 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title="Preventive Maintenance App", layout="centered")
 
-st.title("🛠️ Preventive Maintenance System")
+st.title("🛠️ Digital Maintenance Management System")
 st.markdown("""
-Welcome to the **Preventive Maintenance System**.
-
 Use the sidebar to navigate to:
-- 👨‍🔧 User 1 Page
-- 📘 User 2 Page
+- 👨‍🔧 Master Machine List
+- 📘 Preventive Maintenance
 
 Each user page is login-protected.
 """)
